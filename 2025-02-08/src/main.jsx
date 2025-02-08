b6b3b6e0ca56febfc6cbebb072b050cd931cf761
mateenkhadem <86128603+mateenkhadem@users.noreply.github.com>
2025-02-08 17:42:19 +0330

    This is a simple React Web Page

--- 2025-02-08/src/main.jsx
+++ 2025-02-08/src/main.jsx
@@ -4,7 +4,9 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <div>
+    <img src="https://fastly.picsum.photos/id/799/200/300.jpg?hmac=q6DulPHgFwTpoeoXzeVRLJ7-2cd-K69VyeJoIpUM5eg" />
+    <h1>This is so cool!</h1>
+    <p>I would like to learn more about react</p>
+  </div>,
 )
